7706ea729503de2b3605bba5e3db67b47aabf219
Nguyễn Tấn Dương <dn898322@gmail.com>
2026-02-28 10:49:08 +0700

    Update bai4.c

--- bai4.c
+++ bai4.c
@@ -1,12 +1,48 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Nguyễn Tấn Dương]
+ * MSSV:      [ps47990]
+ * Lớp:       [com108-cs21301]
  *****************************************************************************/
 
 //  BÀI 4: TÍNH BÌNH PHƯƠNG CÁC PHẦN TỬ TRONG MẢNG 2 CHIỀU 
 //  Input: Nhập vào từ bàn phím 1 ma trận các số nguyên. Mảng gồm n hàng, m cột 
 //  Output: Xuất ra màn hình ma trận bình phương 
 
-// VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#define MAX 100 
+int main() {
+    int n, m, matrix[MAX][MAX], squaredMatrix[MAX][MAX];
 
+    // Nhập số hàng và số cột của ma trận
+    printf("Nhap so hang (n <= %d): ", MAX);
+    scanf("%d", &n);
+    printf("Nhap so cot (m <= %d): ", MAX);
+    scanf("%d", &m);
+
+    // Nhập các phần tử của ma trận
+    printf("Nhap cac phan tu cua ma tran:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("matrix[%d][%d] = ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Tính bình phương các phần tử trong ma trận
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            squaredMatrix[i][j] = matrix[i][j] * matrix[i][j];
+        }
+    }
+
+    // Xuất ma trận bình phương
+    printf("Ma tran binh phuong:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", squaredMatrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
